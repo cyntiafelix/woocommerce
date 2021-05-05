a9c0196cca39c4c9362cc22c32f8981d828c4533
cyntiafelix <40671873+cyntiafelix@users.noreply.github.com>
2021-05-04 22:59:06 -0500

    Update products.dart

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -170,7 +170,7 @@ class WooProduct {
         priceHtml = json['price_html'],
         onSale = json['on_sale'],
         purchasable = json['purchasable'],
-        totalSales = json['total_sales'],
+        totalSales = double.tryParse(json['total_sales']) != null ? double.parse(json['total_sales']) : json['total_sales'],
         virtual = json['virtual'],
         downloadable = json['downloadable'],
         downloads = (json['downloads'] as List)
